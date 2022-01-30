e75fe17153bec96f47d7d54fa1d4a799622b77d6
Githubtesi <tesiyosi7211@gmail.com>
2022-01-31 01:44:18 +0900

    abstract sample

---
+++ dependency-inversion-after.py
@@ -0,0 +1,49 @@
+import abc
+
+
+class Switchable(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def turn_on(self):
+        pass
+
+    @abc.abstractmethod
+    def turn_off(self):
+        pass
+
+
+class LightBulb(Switchable):
+    def turn_on(self):
+        print("LightBulb: turned on...")
+
+    def turn_off(self):
+        print("LightBulb: turned off...")
+
+
+class Fan(Switchable):
+    def turn_on(self):
+        print("Fan: turned on...")
+
+    def turn_off(self):
+        print("Fan: turned off...")
+
+
+class ElectricPowerSwitch(object):
+
+    def __init__(self, c: Switchable):
+        self.client = c
+        self.on = False
+
+    def press(self):
+        if self.on:
+            self.client.turn_off()
+            self.on = False
+        else:
+            self.client.turn_on()
+            self.on = True
+
+
+l = LightBulb()
+f = Fan()
+switch = ElectricPowerSwitch(f)
+switch.press()
+switch.press()
